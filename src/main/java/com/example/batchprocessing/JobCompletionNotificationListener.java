2fa022608d9b8a0c9e085f2ba8920e2c56e5e82e
kota-matsuzaki <kota-matsuzaki@DESKTOP-CRJPVB3>
2020-08-17 14:33:30 +0900

    JdbcTemplateを使用したDBアクセス処理が残っていたためMyBatisに変更

--- src/main/java/com/example/batchprocessing/JobCompletionNotificationListener.java
+++ src/main/java/com/example/batchprocessing/JobCompletionNotificationListener.java
@@ -6,7 +6,6 @@ import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,23 +13,15 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
 
 	private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-	private final JdbcTemplate jdbcTemplate;
-
 	@Autowired
-	public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
+	PersonMapper personMapper;
 
 	@Override
 	public void afterJob(JobExecution jobExecution) {
 		if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
 			log.info("!!! JOB FINISHED! Time to verify the results");
 
-			jdbcTemplate.query("SELECT first_name, last_name FROM person",
-				(rs, row) -> new Person(
-					rs.getString(1),
-					rs.getString(2))
-			).forEach(person -> log.info("Found <" + person + "> in the database."));
+			personMapper.findAllPerson().forEach(person -> log.info("Found <" + person + "> in the database."));
 		}
 	}
 }
